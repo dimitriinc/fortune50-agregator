f09827817d3bfdee4fcbbef82fef943eaae3242f
dimitriinc <dimitriinc@proton.me>
2023-06-03 23:26:58 -0500

    webpack config, readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Fortune 50
+
+## Technologies
+- babel-loader 9.1.2
+- css-loader 6.8.1
+- css-minimizer-webpack-plugin 5.0.0
+- jest 29.5.0
+- mini-css-extract-plugin 2.7.6
+- sass 1.62.1
+- sass-loader 13.3.1
+- webpack 5.85.0
+- webpack-cli 5.1.1
+- webpack-dev-server 4.15.0
+- lodash 4.17.21
+
+## License
+This project is licensed under the terms of the MIT license.
--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,16 @@ module.exports = {
     },
     module: {
         rules: [
+    //         {
+    //             test: /\.js$/,
+    //             exclude: /node_modules/,
+    //             use: 'babel-loader'
+    //         },
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /.s?scss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+                // test: /.s?scss$/,
+                // use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             }
         ]
     },
