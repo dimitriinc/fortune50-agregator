24ca7729434e53c452223e69dbeff7e32d149356
dimitriinc <dimitriinc@proton.me>
2023-06-05 23:31:25 -0500

    APIs to get a company's overview

---
+++ src/js/conifg.js
@@ -0,0 +1,6 @@
+const apiKeys = {
+    financialModelingPrepApiKey: '3b66c1e37986a93ef69f5159f9cdb4f7',
+    alphaVantageApiKey: 'GY4WEZIJONUE95HJ',
+}
+
+export default apiKeys
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../style.css'
 import _ from 'lodash'
+import apiKeys from './conifg'
 
 
 function component() {
@@ -10,3 +11,17 @@ function component() {
 }
 
 document.body.appendChild(component())
+
+async function getCompanyProfile(symbol) {
+    const response = await fetch(`https://financialmodelingprep.com/api/v3/profile/${symbol}?apikey=${apiKeys.financialModelingPrepApiKey}`)
+    const data = await response.json()
+    console.log(data)
+}
+
+async function getCompanyOverview(symbol) {
+    const response = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${apiKeys.alphaVantageApiKey}`)
+    const data = await response.json()
+    console.log(data)
+}
+
+getCompanyOverview('IBM')
